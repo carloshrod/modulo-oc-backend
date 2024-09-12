7845958c8afcc415cf196aca466b6e9f73389800
chrod <chrod.dev@gmail.com>
2024-09-11 22:36:23 -0500

    Add nodemailer config

---
+++ src/utils/nodemailer.js
@@ -0,0 +1,14 @@
+import nodemailer from "nodemailer";
+import { config } from "dotenv";
+config();
+
+export const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
+  },
+  tls: {
+    rejectUnauthorized: false, // Remover en producción
+  },
+});
